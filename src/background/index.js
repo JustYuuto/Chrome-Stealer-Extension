9e7ac3bd2e5175771b48f9856241a734cf514bd7
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-30 18:14:44 +0200

    send to webhook

---
+++ src/background/index.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener(async (message) => {
+  if (!message) return;
+
+  const { type, data } = message;
+  if (type === 'SEND_TO_WEBHOOK') {
+    await fetch('', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+});
