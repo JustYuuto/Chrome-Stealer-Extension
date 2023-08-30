600c0854aa93b64d6b945609b185c483714e7521
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-30 19:28:37 +0200

    added google

--- src/content-script/index.js
+++ src/content-script/index.js
@@ -1,4 +1,6 @@
 const discord = require('../websites/discord');
+const google = require('../websites/google');
 const Util = require('../../utils');
 
 Util.Website.init(discord);
+Util.Website.init(google);
