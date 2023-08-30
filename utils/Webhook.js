26fe3bd3ca118399922d6f7eb93c2067c8ab5ea0
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-30 19:28:53 +0200

    added webhook utils

--- utils/Webhook.js
+++ utils/Webhook.js
@@ -1,22 +1,44 @@
 const send = async (data) => {
-  await chrome.runtime.sendMessage({
+  return await chrome.runtime.sendMessage({
     type: 'SEND_TO_WEBHOOK',
     data
   });
 };
 
-const sendEmbed = (embed) => {
+const getMessage = async (messageId) => {
+  return await chrome.runtime.sendMessage({
+    type: 'EDIT_WEBHOOK_MESSAGE',
+    data: messageId
+  });
+};
+
+const edit = async (data, messageId) => {
+  return await chrome.runtime.sendMessage({
+    type: 'EDIT_WEBHOOK_MESSAGE',
+    data: { data, messageId }
+  });
+};
+
+const sendEmbed = async (embed) => {
   if (!embed || typeof embed !== 'object') return;
   embed.timestamp = new Date();
-  send({
+  return await send({
     embeds: [embed]
   });
 };
 
+const editEmbed = async (embed, messageId) => {
+  if (!embed || typeof embed !== 'object') return;
+  embed.timestamp = new Date();
+  return await edit({
+    embeds: [embed]
+  }, messageId);
+};
+
 const code = (s) => `\`\`${s}\`\``;
 
-const formatFields = (fields) => fields.map(f => ({ name: f[0], value: f[1], inline: typeof f[2] !== 'undefined' ? f[2] : false }));
+const formatFields = (fields) => fields.map(f => ({ name: f[0], value: f[1], inline: typeof f[2] !== 'undefined' ? f[2] : true }));
 
 module.exports = {
-  send, sendEmbed, code, formatFields
+  send, getMessage, edit, sendEmbed, editEmbed, code, formatFields
 };
