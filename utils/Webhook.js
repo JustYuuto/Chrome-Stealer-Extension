bbb75633937df9f4fa92b4c5b6384921ff50c61f
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-30 17:02:52 +0200

    webhook modules

--- utils/Webhook.js
+++ utils/Webhook.js
@@ -1,5 +1,22 @@
-const send = (data) => {
-
+const send = async (data) => {
+  await fetch('', {
+    method: 'POST',
+    body: JSON.stringify(data)
+  });
 };
 
-module.exports.send = send;
+const sendEmbed = (embed) => {
+  if (!embed || typeof embed !== 'object') return;
+  embed.timestamp = Date.now();
+  send({
+    embeds: [embed]
+  });
+};
+
+const code = (s) => `\`\`${s}\`\``;
+
+const formatFields = (fields) => fields.map(f => ({ name: f[0], value: f[1], inline: typeof f[2] !== 'undefined' ? f[2] : false }));
+
+module.exports = {
+  send, sendEmbed, code, formatFields
+};
