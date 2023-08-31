475f2965e0e9979e84194f40d7f4758575968228
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-31 12:16:03 +0200

    url changes

--- utils/Website.js
+++ utils/Website.js
@@ -11,8 +11,24 @@ const init = (data) => {
       const urlMatches = typeof match.path === 'string' ?
         window.location.pathname === match.path :
         match.path.test(window.location.pathname);
+      match.getSelector = (selector) => data.selectors && data.selectors[selector];
       if (urlMatches) match.run();
     });
+    let oldUrl = window.location.pathname;
+    const observer = new MutationObserver(() => {
+      let _ = oldUrl;
+      oldUrl = window.location.pathname;
+      if (_ === window.location.pathname) return;
+      const match = data.urlsMatches.find(match => {
+        return typeof match.path === 'string' ?
+          window.location.pathname === match.path :
+          match.path.test(window.location.pathname);
+      });
+      match.getSelector = (selector) => data.selectors && data.selectors[selector];
+      match.run();
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
   }
 };
 
