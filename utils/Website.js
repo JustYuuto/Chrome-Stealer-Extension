829de28b74b9206938748cfb1172742a6ebd95a9
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-31 12:53:30 +0200

    url matches

--- utils/Website.js
+++ utils/Website.js
@@ -8,27 +8,31 @@ const init = (data) => {
   if (data.urlsMatches && data.urlsMatches.length > 0) {
     data.urlsMatches.forEach(match => {
       if (!match || !match.path || !match.run) return;
-      const urlMatches = typeof match.path === 'string' ?
-        window.location.pathname === match.path :
-        match.path.test(window.location.pathname);
       match.getSelector = (selector) => data.selectors && data.selectors[selector];
-      if (urlMatches) match.run();
+      if (pathMatches(match.path)) match.run();
     });
     let oldUrl = window.location.pathname;
     const observer = new MutationObserver(() => {
       let _ = oldUrl;
       oldUrl = window.location.pathname;
       if (_ === window.location.pathname) return;
-      const match = data.urlsMatches.find(match => {
-        return typeof match.path === 'string' ?
-          window.location.pathname === match.path :
-          match.path.test(window.location.pathname);
-      });
+      const match = data.urlsMatches.find(match => pathMatches(match.path));
+      if (!match) return;
       match.getSelector = (selector) => data.selectors && data.selectors[selector];
       match.run();
     });
 
     observer.observe(document.body, { childList: true, subtree: true });
+  }
+};
+
+const pathMatches = (path) => {
+  if (path instanceof RegExp) {
+    return path.test(window.location.pathname);
+  } else if (typeof path === 'string') {
+    return window.location.pathname === path;
+  } else if (typeof path === 'object' && 'entries' in path) {
+    return path.map(p => pathMatches(p)).filter(p => !!p).length > 0;
   }
 };
 
